20113b3baf81e5373a98d137d20024b6b0fb1f48
agregor <agregor@dcsrv-test-ect.ad.unil.ch>
2024-11-12 13:00:50 +0100

    New

---
+++ IMOD_preprocessing/Secret.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo "
+
+NNNNNNNNN      NNNNNNN        AAAAAAAAA VVVVVVV                VVVVVVV
+N::::::::N     N:::::N       A:::::::::A V:::::V  V       V   V:::::V
+N:::::::::N    N:::::N      A:::::A:::::A V:::::V   V   V    V:::::V 
+N::::::::::N   N:::::N     A:::::A A:::::A V:::::V    V     V:::::V
+N::::::N::::N  N:::::N    A:::::A   A:::::A V:::::V       V:::::V
+N:::::N N::::N N:::::N   A:::::A     A:::::A V:::::V     V:::::V
+N:::::N  N::::N::::::N  A:::::AAAAAAAAA:::::A V:::::V   V:::::V
+N:::::N   N::::::::::N A:::::::::::::::::::::A V:::::V V:::::V
+N:::::N    N:::::::::N A::::AAAAAAAAAAAAA::::A  V:::::V:::::V
+N:::::N     N::::::::N A::::A           A::::A   V:::::::::V 
+N:::::N      N:::::::N A::::A           A::::A    V:::::::V 
+NNNNNNN       NNNNNNNN A::::A           A::::A     V:::::V          
+                      A:::::::A        R::::::::::::::R   R::::::::::::::R      OOOOOOOOOOOOO   
+   .        .        A:::::::::A       R::::RRRRRR:::::R  R::::RRRRRR:::::R   OO:::::::::::::OO 
+    \      /        A:::::A:::::A      R::::R     R:::::R R::::R     R:::::R O:::::::OOO:::::::O
+    _\/  \/_       A:::::A A:::::A     R::::R     R:::::R R::::R     R:::::R O::::::O   O::::::O
+ \   _\/\/_   /   A:::::A   A:::::A    R::::R     R:::::R R::::R     R:::::R O:::::O     O:::::O
+ _\_\_\/\/_/_/_  A:::::A     A:::::A   R::::RRRRRR:::::R  R::::RRRRRR:::::R  O:::::O     O:::::O
+  / /_/\/\_\ \  A:::::AAAAAAAAA:::::A  R:::::::::::::RR   R:::::::::::::RR   O:::::O     O:::::O
+ /   _/\/\_   \A:::::::::::::::::::::A R::::RRRRRR:::::R  R::::RRRRRR:::::R  O:::::O     O:::::O
+     /\  /\    A::::AAAAAAAAAAAAA::::A R::::R     R::::R  R::::R     R::::R  O::::::O   O::::::O
+    /      \   A::::A           A::::A R::::R     R::::R  R::::R     R::::R  O:::::::OOO:::::::O
+   '        '  A::::A           A::::A R::::R     R::::R  R::::R     R::::R   OO:::::::::::::OO 
+               AAAAAA           A::::A RRRRRR     R::::R  RRRRRR     RRRRRR     OOOOOOOOOOOOO   
+LLLLLLLLLL                    A:::::::A        B:::::::::::::B  
+ LL::::LL                    A:::::::::A       B::::BBBBBB:::::B      
+  L::::L                    A:::::A:::::A      B::::B     B:::::B
+  L::::L                   A:::::A A:::::A     B::::B     B:::::B
+  L::::L                  A:::::A   A:::::A    B::::BBBBBB:::::B 
+  L::::L                 A:::::A     A:::::A   B:::::::::::::BB  
+  L::::L                A:::::AAAAAAAAA:::::A  B::::BBBBBB:::::B 
+  L::::L               A:::::::::::::::::::::A B::::B     B:::::B
+  L::::L         LLLLL A::::AAAAAAAAAAAAA::::A B::::B     B:::::B
+LL::::::LLLLLLLLL::::L A::::A           A::::A B::::BBBBBB::::::B
+L::::::::::::::::::::L A::::A           A::::A B:::::::::::::::B 
+LLLLLLLLLLLLLLLLLLLLLL AAAAAA           AAAAAA BBBBBBBBBBBBBBBB   
+
+"
